58605e372f959b8f539c0095cd844e541ea5821a
SIRAJ V K <sirajvk@encyclokidia.com>
2018-02-10 19:43:55 -0800

    changes noted

--- LowCost/LowCost/AppDelegate.swift
+++ LowCost/LowCost/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    //siraj test added for jenkins
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
